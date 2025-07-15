dcfae8d4298f5b9af8361659e0ea3416a28e4d03
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-07-15 15:41:08 +0530

    Minor update

--- ai-job-match-frontend/src/api/axios.js
+++ ai-job-match-frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-    baseURL: 'http://65.2.71.241:3001/api',
+    baseURL: 'https://65.2.71.241:3001/api',
 })
 
 export default instance;
